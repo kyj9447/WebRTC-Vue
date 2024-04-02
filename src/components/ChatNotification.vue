06b27e6df651c121d2df372bfd9285db751a966e
Jin.K.001 <kyj9447@gmail.com>
2024-04-02 23:09:39 +0900

    모바일화면 채팅 알림 메세지 애니메이션 수정 TODO: 채팅 알림 사라지는 애니메이션 시작지점 수정

--- src/components/ChatNotification.vue
+++ src/components/ChatNotification.vue
@@ -3,32 +3,42 @@ import { chatNotificationStore } from '../stores/store'
 </script>
 
 <template>
-  <TransitionGroup name="list" tag="ul" mode="out-in">
+  <TransitionGroup name="list" tag="ul" mode="in-out">
     <li
       v-for="(message, index) in chatNotificationStore().$state.notifications"
       :key="index"
       class="noti bg-yellow-200 rounded-2xl p-2 pl-3 m-2 border border-yellow-300"
-    >
-      <span v-html="message.split(',', 2)[1].split(':').join(':<br/>')"></span>
-    </li>
+      v-html="message.split(',', 2)[1].split(':').join(':<br/>')"
+    ></li>
   </TransitionGroup>
 </template>
 
 <style scoped>
 .list-enter-active {
-  animation: slide-in 0.5s forwards;
+  animation: slide-in 0.5s;
 }
 .list-leave-active {
   position: absolute;
+  animation: slide-out 0.5s;
 }
 @keyframes slide-in {
   0% {
     opacity: 0;
-    transform: translateY(100px);
+    transform: translateX(100px);
   }
   100% {
     opacity: 1;
-    transform: translateY(0px);
+    transform: translateX(0px);
+  }
+}
+@keyframes slide-out {
+  0% {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+  100% {
+    opacity: 0;
+    transform: translateX(100px);
   }
 }
 </style>
