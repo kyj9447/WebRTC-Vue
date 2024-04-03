9749c47a4964211e0c2e81115908ada7ce8ba06d
kyj9447 <kyj9447@gmail.com>
2024-04-03 23:37:40 +0900

    dist 추가 및 기타 설정파일 추가

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  plugins: [],
+}
+
